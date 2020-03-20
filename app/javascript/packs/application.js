d59b98e02131a930621231e62e750b4ae8d4e4f2
James Devereux <devereuxjj@gmail.com>
2020-03-20 12:28:09 +0900

    added layouts and bootstap template

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,14 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
+
+import 'assets/javascripts/scripts.js'
+import 'assets/stylesheets/styles.css'
+
+import '@fortawesome/fontawesome-free/js/all'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,51 @@
 Rails.application.routes.draw do
 
-  devise_for :students
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'courses/index'
+  # get 'courses/:id', to: 'courses#show', as: :course
+  resources :courses do
+    get 'lessons/preview', to: 'lessons#preview'
+    resources :lessons, only: [ :index, :new, :create ]    
+    resources :enrollments, only: [ :index ] do 
+      collection { post :import }
+    end
+    resources :lessons_schedules, only: [ :create, :update ]
+  end
+
+
+  resources :lessons_schedules, only: [ :destroy ]
+
+  resources :lessons, only: [ :show, :edit, :update, :destroy ] do
+    get 'setup', to: 'lessons#setup'
+  end
+  resources :course_periods, only: [ :create, :update, :destroy ]
+
+  resources :schools, only: [ :new, :create, :edit, :update] do
+    resources :faculties, only: [ :index, :new, :edit, :create, :update ]
+  end
+
+  resources :faculties, only: [] do 
+    resources :periods, only: [ :create, :update]
+  end
+
+  resources :periods, only: [ :destroy ]
+
+
+  resources :semesters, only: [ :new, :create, :edit, :update]
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  devise_for :students, controllers: { 
+    sessions: 'students/sessions',
+    registrations: 'students/registrations' 
+  } 
+
+  root to: 'pages#home'
+
+  get 'pages/login', to: 'pages#login'
+  get 'pages/signup', to: 'pages#signup'
+  get 'pages/dashboard', to: 'pages#dashboard'
 end
